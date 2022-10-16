75f55aa92ad44e392fa8469067ad170839c7294c
Val Dzhadan <dzhadan.valentyna.w@gmail.com>
2022-10-16 19:37:13 +0300

    Fixing js mobile menu

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,23 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    docBody: document.querySelector('body'),
+
+    headerBtn: document.querySelector('.header__btn'),
+    heroBtn: document.querySelector('.hero__btn'),
+    offeringsBtn: document.querySelector('.offerings__btn'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  refs.headerBtn.addEventListener('click', toggleModal);
+  refs.heroBtn.addEventListener('click', toggleModal);
+  refs.offeringsBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
+    refs.docBody.classList.toggle('_lock');
   }
 })();
